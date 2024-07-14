894c08034a841cccb18498ae8565137400c0d0f8
Tejas Shah <tejasshah2k19@gmail.com>
2024-07-14 12:41:23 +0530

    sort task

--- DMAStructStudentArray.c
+++ DMAStructStudentArray.c
@@ -20,17 +20,40 @@ int main()
     scanf("%d", &totalStudents);
     s = calloc(totalStudents, sizeof(struct student)); // 79
 
+    // X 33
+    // Y 22
+    // Z 11
+
     for (i = 0; i < totalStudents; i++)
     {
 
         printf("\nEnter name batch Rank and colgName");
         scanf("%s%s%d%s", &s[i].name, &s[i].batch, &s[i].rank, &s[i].colgName);
     }
+    // X 33
+    // Y 22
+    // Z 11
+
     for (i = 0; i < totalStudents; i++)
     {
         printf("\n**********\n");
         printf("\nName = %s\nBatch = %s\nRank = %d\nColgName = %s", s[i].name, s[i].batch, s[i].rank, s[i].colgName);
     }
+
+    // some code goes here
+
+    // Z 11
+    // Y 22
+    // X 33
+
+    for (i = 0; i < totalStudents; i++)
+    {
+        printf("\n**********\n");
+        printf("\nName = %s\nBatch = %s\nRank = %d\nColgName = %s", s[i].name, s[i].batch, s[i].rank, s[i].colgName);
+    }
+    // X 33
+    // Y 22
+    // Z 11
 
     free(s);
     return 0;
