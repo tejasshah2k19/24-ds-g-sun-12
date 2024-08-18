ec3c9e8e023d86b1d2bf21c41dfecdcaf24ec169
Tejas Shah <tejasshah2k19@gmail.com>
2024-08-18 12:27:28 +0530

    linked list - 1

--- LinkedList.c
+++ LinkedList.c
@@ -29,22 +29,81 @@ void addNode(int num)
     }
 }
 
+void addNodeBeg(int num)
+{
+    struct node *tmp;
+    if (head == NULL)
+    {
+        addNode(num);
+    }
+    else
+    {
+        tmp = malloc(sizeof(struct node));
+        tmp->data = num;
+        tmp->next = head;
+        head = tmp;
+    }
+}
+void deleteAtEnd()
+{
+    struct node *p;
 
-void display(){
-   struct node *i = head ; 
+    for (p = head; p->next != last; p = p->next);
+    p->next = NULL;     
+    free(last);
+    last  = p;
+}
+void display()
+{
+    struct node *i;
 
-    for(i=head; i!=NULL ;i=i->next){
-        printf(" %d",i->data);
+    for (i = head; i != NULL; i = i->next)
+    {
+        printf(" %d", i->data);
     }
 }
 
 int main()
 {
-    addNode(10); // head
-    addNode(20); // second
-    addNode(30);
-    addNode(40);
+    int choice;
+    int num;
+    while (404)
+    {
+        printf("\n1 For Add Node + END");
+        printf("\n2 For Add Node + BEG");
+        printf("\n3 For Add Node + ANY");
 
-    display();
+        printf("\n4 For Delete Node - END");
+        printf("\n5 For Delete Node - BEG");
+        printf("\n6 For Delete Node - ANY");
+
+        printf("\n7 For Display LinkedList");
+        printf("\n0 EXIT\n");
+
+        printf("\nEnter Your Choice");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter number");
+            scanf("%d", &num);
+            addNode(num);
+            break;
+        case 2:
+            printf("\nEnter number");
+            scanf("%d", &num);
+            addNodeBeg(num);
+            break;
+        case 4:
+            deleteAtEnd();
+            break;
+        case 7:
+            display();
+            break;
+        case 0:
+            exit(0);
+        }
+    }
     return 0;
 }
